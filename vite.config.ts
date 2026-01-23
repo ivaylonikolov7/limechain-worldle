ac1ac9ce97d9f88eff22e264278ecb0d61af1b69
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2026-01-23 03:24:08 +0200

    fix: pathing
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/limedle/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
